885e450e36c5d3a82bc4b08c102bf60bbb636530
Muhammad Rizki Aiman <53157683+ashirogisen@users.noreply.github.com>
2019-08-02 15:26:43 +0700

    Update js_if-else.js

--- js_if-else.js
+++ js_if-else.js
@@ -2,25 +2,24 @@ var nama = "Iki";
 var peran = "Tabib";
 
 if (nama.length !== 0 && peran.length !==0) {
-	console.log('Selamat datang di Dunia Proxytia, ' + nama);
-	  if (peran === 'Ksatria') {
-		  console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    if (peran === 'Ksatria') {
+	console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
     } 
     else if (peran === 'Tabib') {
-		  console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
+	console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
     } 
     else if (peran === 'Penyihir') {
-		  console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+	console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
     }
     else {
-      console.log("Data yang anda masukkan invalid");
-        
+        console.log("Data yang anda masukkan invalid");
     }
 }
     else if (nama.length !== 0 && peran.length === 0) {
-	  	console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+	console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
     }
 
 else {
-  console.log('Nama harus diisi!');
+   console.log('Nama harus diisi!');
 }
